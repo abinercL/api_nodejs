31a7e7c3f29ce35abdbaeae385a0faa96ff86657
abinercL <abiner17costa@gmail.com>
2024-07-25 12:40:53 -0300

    standard routes created

--- server.js
+++ server.js
@@ -13,9 +13,25 @@ import { fastify } from "fastify";
 
 
 const server = fastify()
-
-server.get('/', () => {
+//criando uma rota que permita criar
+server.post('/videos', () => {
     return 'hello word'
+})
+
+//criando rota que permite pegar informaçoes por exemplo
+server.get('/videos', () => {
+    return 'hello playng'
+})
+
+//rota que me permite atualizar informaçoes via normalmente ID
+//chamado de route parameter ex: ID
+server.put('/videos/:id', () => {
+    return 'hello play'
+})
+
+//rota na qual permite deletar uma escolha por vez por meio por exemplo de um ID
+server.delete('/videos/:id', () => {
+    return 'hello yng'
 })
 server.listen({
     port: 3333
