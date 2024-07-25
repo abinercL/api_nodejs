9f8af1ebf9566805875f7841b1c838c11b882598
abinercL <abiner17costa@gmail.com>
2024-07-25 12:53:24 -0300

    Created database in memory

---
+++ database_memory.js
@@ -0,0 +1,22 @@
+import { randomUUID } from "node:crypto"
+export class DatabaseMemory {
+
+
+    #videos = new Map()
+    list() {
+        this.#videos.values()
+    }
+
+    create(video) {
+        const videoId = randomUUID()
+        this.#videos.set(videoId)
+    }
+
+    update(id, video) {
+        this.#videos.set(id, video)
+    }
+
+    delete(id) {
+        this.#videos.delete(id, video)
+    }
+}
--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-/* import { createServer } from 'node:http'
-
-
-const server = createServer((request, response) => { //servidor criado, recebendo duas funçoes response e requeste
-    response.write("Hello World")
-
-    return response.end()
-})
-//sendo ouvido na porta 3333 
-server.listen(3333) */
-
 import { fastify } from "fastify";
-
+import { DatabaseMemory } from "./database_memory.js";
 
 const server = fastify()
+
 //criando uma rota que permita criar
 server.post('/videos', () => {
     return 'hello word'
